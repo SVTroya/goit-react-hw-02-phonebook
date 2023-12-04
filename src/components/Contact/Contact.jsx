fd688bd7f9c4cc78ac8a42706bdb18bd0989a4e3
SVTroya <my.svtroy@gmail.com>
2023-12-04 19:12:27 +0200

    Homework#2 Task #2 (part 5-6)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,24 @@
-import { Component } from 'react';
 import { IconWrapper, Name, Phone } from './Contact.styled';
 import PropTypes from 'prop-types';
 import { ImBin } from 'react-icons/im';
 
-export class Contact extends Component {
+export function Contact({contact: {id, name, phone}, onRemoveContact}) {
 
-static propTypes = {
+  return (
+    <>
+      <Name>{name}</Name>
+      <Phone>tel.:{phone}</Phone>
+      <IconWrapper>
+        <ImBin size={20} onClick={() => onRemoveContact(id)} />
+      </IconWrapper>
+    </>
+  )
+}
+
+Contact.propTypes = {
   contact: PropTypes.shape({
     id: PropTypes.string,
     name: PropTypes.string,
   }),
   onRemoveContact: PropTypes.func,
-}
-
-  render() {
-  const {id, name, phone} = this.props.contact
-    return (
-      <>
-        <Name>{name}</Name>
-        <Phone>tel.:{phone}</Phone>
-        <IconWrapper>
-          <ImBin size={20} onClick={() => this.props.onRemoveContact(id)}/>
-        </IconWrapper>
-      </>
-    )
-  }
 }
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,30 +1,19 @@
-import { Component } from 'react';
 import { FilterInputStyled } from './FilterInputStyled';
 import PropTypes from 'prop-types';
 
-export class Filter extends Component {
-
-  static propTypes = {
-    filter: PropTypes.string,
-    onChange: PropTypes.func,
-  }
-
-  filterInputId = crypto.randomUUID();
-
-  handleChange = ({ target: input }) => {
-    const { name: inputName, value } = input;
-    this.props.onChange(inputName, value);
-  };
-
-  render() {
-    return (
-      <FilterInputStyled
-        type='text'
-        name='filter'
-        id={this.filterInputId}
-        value={this.props.filter}
-        placeholder='Find conacts by name'
-        onChange={this.handleChange} />
-    )
-  }
+export function Filter({ filter, onChange }) {
+  return (
+    <FilterInputStyled
+      type='text'
+      name='filter'
+      id={crypto.randomUUID()}
+      value={filter}
+      placeholder='Find conacts by name'
+      onChange={({ target: { name, value } }) => onChange(name, value)} />
+  );
 }
+
+Filter.propTypes = {
+  filter: PropTypes.string,
+  onChange: PropTypes.func,
+};
